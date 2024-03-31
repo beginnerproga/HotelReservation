98fefd6f6415c08586b77ff3fd13d2367b80c7d2
NDKim <NDKim@lanit.ru>
2024-03-31 23:21:54 +0300

    UPDATE_MIGRATIONS_FILES

---
+++ src/main/resources/db/migration/V_01_8__INSERT_INTO_ROOMS.sql
@@ -0,0 +1,15 @@
+INSERT INTO rooms (type, price, capacity, hotel_id)
+VALUES ('DELUXE', 1200, 3, 4),
+       ('LUXURY', 2500, 4, 4),
+       ('LUXURY', 5000, 5, 4),
+       ('DELUXE', 1500, 2, 5),
+       ('LUXURY', 3000, 4, 5),
+       ('LUXURY', 4500, 6, 5),
+       ('STANDARD', 1000, 2, 6),
+       ('DELUXE', 2000, 3, 6),
+       ('LUXURY', 3500, 4, 6),
+       ('ECONOMY', 800, 2, 7),
+       ('STANDARD', 1200, 2, 7),
+       ('DELUXE', 1800, 3, 7);
+ON CONFLICT (hotel_id, type) DO NOTHING;
+
